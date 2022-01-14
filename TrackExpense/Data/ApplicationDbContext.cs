0427e90c6ffd0bd38b621ba9344dd8643ef16653
Balasubramanya Bharadwaj <Bharadwaj.BS@outlook.com>
2022-01-14 16:27:55 +0530

    Home page changes

--- TrackExpense/Data/ApplicationDbContext.cs
+++ TrackExpense/Data/ApplicationDbContext.cs
@@ -3,14 +3,45 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TrackExpense.Models;
 
 namespace TrackExpense.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ExpenseDetail> ExpenseDetails { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ExpenseDetail>(entity =>
+            {
+                entity.Property(e => e.UserEmail)
+                .IsRequired();
+
+                entity.Property(e => e.Date).HasColumnType("date")
+                .IsRequired();
+
+                entity.Property(e => e.ExpenseDescription)
+                    .HasMaxLength(250)
+                    .IsUnicode(false)
+                    .IsRequired();
+
+                entity.Property(e => e.Amount).HasColumnType("Amount")
+                .IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        void OnModelCreatingPartial(ModelBuilder modelBuilder) { }
     }
 }
---
+++ TrackExpense/Models/ExpenseDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrackExpense.Models
+{
+    public class ExpenseDetail
+    {
+        [Required]
+        [DisplayName("User Email")]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [DisplayName("Spent On")]
+        public string ExpenseDescription { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
